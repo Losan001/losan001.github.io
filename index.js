88ac465562dbbe17910a119304178fc0adc29871
jiangjiang <12463gguser@gmail.com>
2020-05-27 01:55:49 +0800

    init

---
+++ index.js
@@ -0,0 +1,37 @@
+function getQueryObj() {
+    var obj = {};
+    var str = decodeURI(location.search.replace('?', ''));
+    var arr = str.split('&');
+    arr.forEach(function (value) {
+        let k = value.match(/(.+)=.+|$/)[1];
+        let v = decodeURI(value.match(/.+=(.+)|$/)[1]);
+        if (k && v) obj[k] = v;
+    });
+    return obj;
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+    Reveal.initialize({
+        width: 960,
+        height: 700,
+
+        // Factor of the display size that should remain empty around the content
+        margin: 0.1,
+
+        // 显示页码
+        slideNumber: true,
+        // Bounds for smallest/largest possible scale to apply to content
+        minScale: 0.2,
+        maxScale: 1.5,
+
+        history: true,
+
+        dependencies: [
+            { src: 'plugin/markdown/marked.js' },
+            { src: 'plugin/markdown/markdown.js' },
+            { src: 'plugin/notes/notes.js', async: true },
+            { src: 'plugin/highlight/highlight.js', async: true, callback: function () { hljs.initHighlightingOnLoad(); } }
+        ]
+    });
+
+}, false)
